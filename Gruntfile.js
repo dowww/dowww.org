ffa3fa2a6a31b018d271d4a432ba89678ada6ba8
Rodrigo Willrich <rodrigo.willrich@hotmail.com>
2014-08-17 21:41:11 -0300

    Refactor Gruntfile.js using load-grunt-config task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,57 +1,7 @@
-/* global module:false */
-
 module.exports = function(grunt) {
 
-  // Project configuration
-  grunt.initConfig({
+  require('load-grunt-config')(grunt);
 
-    // Tasks configuration
-    modernizr: {
-      dist: {
-        devFile: 'vendor/assets/components/modernizr/modernizr.js',
-        outputFile: 'vendor/assets/javascripts/modernizr-custom.js',
-        parseFiles: true,
-        files: {
-          src: ['app/assets/**/*.+(scss|css|js)']
-        },
-        uglify: false,
-        extra: {
-          mq: true
-        },
-        extensibility: {
-          prefixed: true
-        }
-      }
-    },
-
-    watch: {
-      options: {
-        livereload: true
-      },
-      railsAssets: {
-        files: 'app/assets/**/*.+(scss|css|js|png|jpg|svg)'
-      },
-      rubyFiles: {
-        files: 'app/**/*.rb'
-      },
-      railsLocales: {
-        files: 'config/locales/**/*.yml'
-      },
-      railsStaticFiles: {
-        files: 'public/**/*.+(css|html|js|txt)'
-      },
-      railsViews: {
-        files: 'app/views/**/*.+(slim|erb)'
-      }
-    }
-
-  });
-
-  // Tasks load
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-modernizr");
-
-  // Tasks declaration
   grunt.registerTask('default', ['modernizr']);
 
 };
