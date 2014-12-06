378c793181bfa9a3fb5205d51ce0bf0c4ffb9d9e
Rodrigo Willrich <rodrigo.willrich@hotmail.com>
2014-12-06 17:41:07 -0200

    Update to Rails 4.2 RC

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.1.5'
 
-gem 'rails', '~> 4.1.4'
+gem 'rails', '~> 4.2.0.rc2'
 
 gem 'pg', '~> 0.17.1'
 
@@ -14,10 +14,13 @@ gem 'bourbon', '~> 3.2.3'
 gem 'requirejs-rails', '~> 0.9.4'
 gem 'uglifier', '~> 2.5.3'
 
+gem 'responders', '~> 2.0.2'
+
 gem 'rails-i18n', '~> 4.0.2'
 
 group :development do
-  gem 'spring', '~> 1.1.3'
+  gem 'web-console', '~> 2.0.0'
+  gem 'spring', '~> 1.2.0'
   gem 'spring-commands-rspec', '~> 1.0.2'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141206190811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: true do |t|
+    t.string   "title",      limit: 255, null: false
+    t.text     "summary"
+    t.text     "text",                   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
